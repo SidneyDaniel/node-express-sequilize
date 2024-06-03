52aa895a3482472d14a0912745f8965bd610c31b
Sid.Daniel <sidneylopes17@gmail.com>
2024-06-03 16:00:43 -0300

    Regatoração da código da base de dados

--- src/controllers/Controller.js
+++ src/controllers/Controller.js
@@ -8,7 +8,7 @@ class Controller {
       const listaDeRegistros = await this.entidadeService.pegaTodosOsRegistros();
       return res.status(200).json(listaDeRegistros);
     } catch (erro) {
-      //erro
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
@@ -18,7 +18,7 @@ class Controller {
       const umRegistro = await this.entidadeService.pegaUmRegistroPorId(Number(id));
       return res.status(200).json(umRegistro);
     } catch (erro) {
-      // erro
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
@@ -34,7 +34,7 @@ class Controller {
 
       return res.status(200).json({ mensagem: 'atualizado com sucesso' });
     } catch (erro) {
-      //erro
+      return res.status(500).json({ erro: erro.message });
     }
   }
 
@@ -44,7 +44,7 @@ class Controller {
       const novoRegistroCriado = await this.entidadeService.criaRegistro(dadosParaCriacao);
       return res.status(200).json(novoRegistroCriado);
     } catch (erro) {
-      // erro
+      return res.status(500).json({ erro: erro.message });
     }
   }
   
@@ -55,8 +55,8 @@ class Controller {
       return res.status(200).json({ mensagem: `id ${id} deletado` });
 
 
-    } catch (error) {
-      return res.status(500).json(error.message);
+    } catch (erro) {
+      return res.status(500).json(erro.message);
     }
   }
 
--- src/database/models/pessoa.js
+++ src/database/models/pessoa.js
@@ -27,7 +27,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Pessoa',
-    tableName: 'pessoas'
+    tableName: 'pessoas',
+    paranoid: true,
   });
   return Pessoa;
 };
